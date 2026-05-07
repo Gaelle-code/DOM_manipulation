962d05c0f207331364ee4fe82aed3f0845cca715
Gaelle-code <your-email-for-gaelle@example.com>
2026-05-07 14:32:01 +0200

    adding Navigation & Dropdown

---
+++ script.js
@@ -0,0 +1,9 @@
+// 1. Theme Toggle
+document.getElementById('themeToggle').addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
+
+// 2. Text Manipulation
+document.getElementById('changeTextBtn').addEventListener('click', () => {
+    document.getElementById('mainParagraph').innerText = "You've successfully manipulated the DOM!";
+});
